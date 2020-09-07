414202004092e084d7da596f5c0e55475cb5ec12
kathyle-dev <69046536+kathyle-dev@users.noreply.github.com>
2020-09-07 16:54:09 -0400

    added media API

---
+++ js/main.js
@@ -0,0 +1,63 @@
+let media = {
+  btnBook: document.getElementById('btnBook'),
+  btnVideo: document.getElementById('btnVideo'),
+  btnPhoto: document.getElementById('btnPhoto'),
+  btnMore: document.getElementById('btnMore'),
+  photo: document.getElementById('photo'),
+  video: document.getElementById('video'),
+  input: document.getElementById('input'),
+  book: document.getElementById('book'),
+  count: 0,
+  getPhoto(){
+    let input = media.input.value.replace(" ","+");
+      fetch(`https://pixabay.com/api/?key=18152452-ad2ae05e127c7b14eba5e7e37&q=${input}&image_type=photo&per_page=15`)
+        .then(response => response.json())
+        .then(data => {
+            media.photo.classList.remove("hide")
+            media.photo.src= data.hits[media.count].largeImageURL
+            media.video.classList.add("hide")
+            setTimeout(function(){
+              media.btnMore.classList.remove("hide")
+            }, 165)
+      })
+        .catch(err => alert("Out of photos! Try another search."))
+        // media.getRandom()
+  },
+  getVideo(){
+      let input = media.input.value.replace(" ","+");
+      fetch(`https://pixabay.com/api/videos/?key=18152452-ad2ae05e127c7b14eba5e7e37&q=${input}&page=1&per_page=15`)
+        .then(response => response.json())
+        .then(data => {
+            media.video.classList.remove("hide")
+            media.video.src= data.hits[media.count].videos.large.url
+            media.photo.classList.add("hide")
+            setTimeout(function(){
+              media.btnMore.classList.remove("hide")
+            }, 165)
+          })
+            .catch(err => alert("Out of videos! Try another search."))
+            // media.getRandom()
+        },
+    // getRandom(){
+    //   let input = media.input.value.replace(" ","+");
+    //   fetch(`http://openlibrary.org/subjects/${input}.json?details=true?limit=1`)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       media.book.innerHTML =`BOOK: ${data.works[media.count].title}, AUTHOR: ${data.works[media.count].authors[0].name}`
+    //       media.book.classList.remove("hide")
+    //     })
+    //     .catch(err => alert("Out of books! Try another search."))
+    // }
+}
+
+media.btnVideo.addEventListener("click", media.getVideo);
+media.btnPhoto.addEventListener("click", media.getPhoto);
+media.btnMore.addEventListener("click", ()=>{
+  if(media.photo.classList.contains("hide")){
+    media.getVideo();
+    media.count+=1
+  }else if(media.video.classList.contains("hide")){
+    media.getPhoto();
+    media.count+=1
+  }
+});
